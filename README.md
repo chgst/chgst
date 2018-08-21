fb4c872f60d38115d7ddec9de37e72150d5cd26b
Janusz Slota <janusz.slota@nixilla.com>
2018-08-21 14:23:45 +0100

    adds QA tools config

---
+++ README.md
@@ -0,0 +1,14 @@
+# Changeset
+
+[![Version](https://img.shields.io/packagist/v/chgst/chgst.svg?style=flat-square)](https://packagist.org/packages/chgst/chgst)
+[![Build Status](https://travis-ci.org/chgst/chgst.svg?branch=develop)](https://travis-ci.org/chgst/chgst)
+[![Coverage Status](https://coveralls.io/repos/github/chgst/chgst/badge.svg?branch=develop)](https://coveralls.io/github/chgst/chgst?branch=develop)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/chgst/chgst/badges/quality-score.png?b=develop)](https://scrutinizer-ci.com/g/chgst/chgst/?branch=develop)
+[![License](https://poser.pugx.org/chgst/chgst/license.svg)](https://packagist.org/packages/chgst/chgst)
+
+## Installation
+
+```bash
+composer require chgst/chgst
+```
+
--- spec/Changeset/Communication/InMemoryCommandBusSpec.php
+++ spec/Changeset/Communication/InMemoryCommandBusSpec.php
@@ -3,9 +3,11 @@
 namespace spec\Changeset\Communication;
 
 use Changeset\Command\CommandInterface;
+use Changeset\Command\HandlerInterface;
 use Changeset\Communication\CommandBusInterface;
 use Changeset\Communication\InMemoryCommandBus;
 use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
 class InMemoryCommandBusSpec extends ObjectBehavior
 {
@@ -15,8 +17,17 @@ class InMemoryCommandBusSpec extends ObjectBehavior
         $this->shouldHaveType(InMemoryCommandBus::class);
     }
 
-    function it_can_dispatch_commands(CommandInterface $command)
+    function it_can_dispatch_commands(CommandInterface $command, HandlerInterface $handler)
     {
+        $this->addHandler($handler);
+
+        $handler->supports(Argument::any())->willReturn(false);
+
+        $this->dispatch($command);
+
+        $handler->supports(Argument::any())->willReturn(true);
+        $handler->process(Argument::any())->shouldBeCalled();
+
         $this->dispatch($command);
     }
 }
